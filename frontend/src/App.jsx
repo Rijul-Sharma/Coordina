c4bcf523ab1ccd6671f0617aaff733336403604d
Tushar Yadav <tushar6420@gmail.com>
2025-10-16 13:02:44 +0530

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Project from './pages/Project'
-import './App.css'
-=======
 
 
 import { Routes, Route } from 'react-router-dom';
@@ -14,25 +7,15 @@ import SetRolePage from './pages/SetRolePage.jsx';
 
 
 
->>>>>>> c094aa8 (changes)
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      {/* <div>Helloooo</div> */}
-      {/* <ManagerHome /> */}
-      <Project />
-    </>
-  )
-=======
     <Routes>
       <Route path="/" element={<SetRolePage />} />
       <Route path="/manager" element={<ManagerHome />} />
       <Route path="/employee" element={<EmpHome />} />
     </Routes>
   );
->>>>>>> c094aa8 (changes)
 }
   
 export default App;
